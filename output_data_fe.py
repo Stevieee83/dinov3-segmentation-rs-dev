5bf66f15ae497d27024b5f51b9077f4ef5bdf10d
S. Wallace <s.wallace.23@abdn.ac.uk>
2025-09-02 11:49:16 +0100

    Update output_data_fe.py

--- output_data_fe.py
+++ output_data_fe.py
@@ -7,45 +7,33 @@ class OutputDataFE():
        ARGS:
             x_tensor (tensor): tensor of the features X from the DINOv3 feature extractor.
             y_tensor (tensor): tensor of the labels y.
-            idx_tensor (tensor): tensor of the indexes for each image's features and labels.
-            x_tensor_path (str): path to save the X features CSV file to.
-            y_tensor_path (str): path to save the y labels CSV file to.
-            idx_tensor_path (str): path to save the image features indexes CSV file to.
+            x_tensor_path (str): path to save the X features CSV file.
+            y_tensor_path (str): path to save the y labels CSV file.
     """
-    def __init__(self, x_tensor, y_tensor, idx_tensor, x_tensor_path, y_tensor_path, idx_tensor_path):
+    def __init__(self, x_tensor, y_tensor, x_tensor_path, y_tensor_path):
         self.x_tensor = x_tensor
         self.y_tensor = y_tensor
-        self.idx_tensor = idx_tensor
         self.x_tensor_path = x_tensor_path
         self.y_tensor_path = y_tensor_path
-        self.idx_tensor_path = idx_tensor_path
 
-    def tensor_to_df_features(self): 
+    def tensor_to_df_features(self):
 
-        # Create a DataFrame
+        # Convert to NumPy, then DataFrame
         x_df = pd.DataFrame(self.x_tensor.cpu().numpy())
 
         print("Converted DataFrame Shape")
         print(x_df.shape)
 
         x_df.to_csv(self.x_tensor_path, index=False)
-    
+        print('Saved features to file.')
+
     def tensor_to_df_labels(self):
-    
+
         # Convert to NumPy, then DataFrame
         y_df = pd.DataFrame(self.y_tensor.cpu().numpy(), columns=["labels"])
-    
+
         print("Converted DataFrame Shape")
         print(y_df.shape)
 
         y_df.to_csv(self.y_tensor_path, index=False)
-
-    def tensor_to_df_index(self):
-    
-        # Convert to NumPy, then DataFrame
-        idx_df = pd.DataFrame(self.idx_tensor.cpu().numpy(), columns=["index"])
-    
-        print("Converted DataFrame Shape")
-        print(idx_df.shape)
-
-        idx_df.to_csv(self.idx_tensor_path, index=False)
+        print('Saved labels to file.')
